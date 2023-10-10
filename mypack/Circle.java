079350b2e314cdb319fe1c047aa72d454c1f4072
Peter_Torki <peter.j.torki@gmail.com>
2023-10-10 16:36:14 +0300

    myPack

---
+++ mypack/Circle.java
@@ -0,0 +1,21 @@
+package mypack;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+}
