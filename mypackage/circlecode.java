3b8195afdfbf86b522989e9ab36357cbc2a7a38d
Shubham <shubham2003tanwar@gmail.com>
2023-10-10 21:55:09 +0530

    this is the code related to packages

---
+++ mypackage/circlecode.java
@@ -0,0 +1,11 @@
+// Circle.java
+import mypack.MyPackage;
+
+public class Circle {
+    public static void main(String[] args) {
+        System.out.println("Circle class");
+        
+        // Use a method from the mypack package
+        MyPackage.myMethod();
+    }
+}
