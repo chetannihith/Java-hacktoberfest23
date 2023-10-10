bc1266fa8d6b0a199ee4d0b6d732e598629d1935
Peter_Torki <peter.j.torki@gmail.com>
2023-10-10 16:41:20 +0300

    Adding 2 nums using cmd

---
+++ mypack/Add2NumsUsingCmd.java
@@ -0,0 +1,9 @@
+class Add2NumsUsingCmd {
+	public static void main(String args[]) {
+		int a, b, sum;
+		a = Integer.parseInt(args[0]);
+		b = Integer.parseInt(args[1]);
+		sum = a + b;
+		System.out.println("Sum of " + a + " and " + b + " is " + sum);
+	}
+}
