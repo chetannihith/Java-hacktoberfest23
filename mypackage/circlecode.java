f4c67b479df72517dfa5bd99419107276ee5c81a
Shubham <shubham2003tanwar@gmail.com>
2023-10-13 18:22:58 +0530

    updated circlecode

--- mypackage/circlecode.java
+++ mypackage/circlecode.java
@@ -1,11 +1,16 @@
-// Circle.java
-import mypack.MyPackage;
+import mypack.Circle;  // Import the Circle class from the "mypack" package.
 
-public class Circle {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Circle class");
-        
-        // Use a method from the mypack package
-        MyPackage.myMethod();
+        // Create an instance of the Circle class
+        Circle myCircle = new Circle(5.0);
+
+        // Access methods from the Circle class
+        double radius = myCircle.getRadius();
+        double area = myCircle.calculateArea();
+
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + area);
     }
+    
 }
