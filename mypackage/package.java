ed696838ce42c76eb866003571f61b4a21c6f8ae
Shubham <shubham2003tanwar@gmail.com>
2023-10-13 18:09:45 +0530

    packages

---
+++ mypackage/package.java
@@ -0,0 +1,18 @@
+// MyPackage.java
+package mypack;  // This specifies that the class is part of the "mypack" package.
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
