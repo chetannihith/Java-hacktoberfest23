3ec87ba7de2e43d29ba48d448a7d0c808045e557
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-13 02:32:46 +0530

    Create Reverse_Linked_List.java

---
+++ DSA/Reverse_Linked_List.java
@@ -0,0 +1,39 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        
+        ListNode reversedHead = reverseList(head);
+        
+        while (reversedHead != null) {
+            System.out.print(reversedHead.val + " ");
+            reversedHead = reversedHead.next;
+        }
+    }
+}
